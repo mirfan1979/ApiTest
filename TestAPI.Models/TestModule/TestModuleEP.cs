6f492510f6b416435972f4d8207902dc94d13d98
Mujeeb Qureshi <abdul.mujeeb.qureshi@hotmail.com>
2019-11-24 02:56:26 +0500

    changes

--- TestAPI.Models/TestModule/TestModuleEP.cs
+++ TestAPI.Models/TestModule/TestModuleEP.cs
@@ -40,6 +40,43 @@ namespace TestAPI.Models
             
         }
 
+        public static object GetCustomList(RequestModel request)
+        {
+            //var test = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(Convert.ToString(request.RequestData));
+
+            // Setup the connection and compiler
+            var connection = new MySqlConnection(ConfigurationManager.AppSettings["MySqlDBConn"].ToString());
+            var compiler = new MySqlCompiler();
+            var db = new QueryFactory(connection, compiler);
+
+            SuccessResponse successResponseModel = new SuccessResponse();
+
+            try
+            {
+                // You can register the QueryFactory in the IoC container
+                //var response = db.Query("jpadmin").Get();  //db.Query("jpexperience").Where("ExpId", 6).Where("ProfileId", 4).First();
+                //var response = db.Select("select * from jpadmin");
+                string strRawQuery = @"
+                    (select * from jpopening where Department = 'Human Resource' limit 1)
+                    union all
+                    (select * from jpopening where Department = 'Marketing' limit 1)
+                    union all
+                    (select * from jpopening where Department = 'IT' limit 1)
+                ";
+                var response = db.Select(strRawQuery);
+                
+                bool hasData = (response != null) ? true : false;
+                successResponseModel = new SuccessResponse(response, hasData);
+            }
+            catch (Exception ex)
+            {
+                //Logger.WriteErrorLog(ex);
+                return new ErrorResponse(ex.Message, HttpStatusCode.BadRequest);
+            }
+
+            return successResponseModel;
+
+        }
         public static object SaveDemoRecord(RequestModel request)
         {
             var test = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(Convert.ToString(request.RequestData));
