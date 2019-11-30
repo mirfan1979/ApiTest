054d658663b2b6771655986ef0745ec1131b66f1
Ahmed Raza <ahmedrazamehmood@gmail.com>
2019-11-30 17:59:55 +0500

    UserSharePercent

--- TestAPI.Models/TestModule/TestModuleEP.cs
+++ TestAPI.Models/TestModule/TestModuleEP.cs
@@ -81,7 +81,75 @@ namespace TestAPI.Models
             return successResponseModel;
 
         }
+        public static object GetSharePercentage(RequestModel request)
+        {
+            var test = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(Convert.ToString(request.RequestData));
+            object _PropertyID;
+            test.TryGetValue("PropertyID", out _PropertyID);
+            object Amount;
+            test.TryGetValue("Amount", out Amount);
+            int _Amount = Convert.ToInt32(Amount);
+            // Setup the connection and compiler
+            var connection = new MySqlConnection(ConfigurationManager.AppSettings["MySqlDBConn"].ToString());
+            var compiler = new MySqlCompiler();
+            var db = new QueryFactory(connection, compiler);
+            int _totalShareQuantity=0;
+            int _CurrentValue=0; 
+            SuccessResponse successResponseModel = new SuccessResponse();
 
+            try
+            {
+                // You can register the QueryFactory in the IoC container
+                //var response = db.Query("jpadmin").Get();  //db.Query("jpexperience").Where("ExpId", 6).Where("ProfileId", 4).First();
+                //var response = db.Select("select * from jpadmin");
+                IDictionary<string, object>  response = db.Query("PropertyShare")
+                                                          .Where("PropertyID", _PropertyID)
+                                                          .Get()
+                                                          .Cast<IDictionary<string, object>>().First(); ;  //db.Query("jpexperience").Where("ExpId", 6).Where("ProfileId", 4).First();
+                
+                bool hasData = (response != null) ? true : false;
+                if (hasData)
+                {
+                   
+                        _totalShareQuantity = Convert.ToInt32(response["TotalShareQuantity"]);
+                    
+                }
+                string sql = @"select * from propertyprediction where propertyid = {0} and year(predictionyear)=year(current_date()) ;";
+                sql = string.Format(sql, _PropertyID);
+                IDictionary<string, object> predresponse = db.Select(sql).Cast<IDictionary<string, object>>().First();
+
+
+                /*("PropertyPrediction")
+                .Where("PropertyID", _PropertyID).Where()
+                .Get()
+                .Cast<IDictionary<string, object>>().First();*/
+
+                hasData = (response != null) ? true : false;
+                if (hasData)
+                {
+
+                   _CurrentValue = Convert.ToInt32(predresponse["CurrentValue"]);
+
+                }
+                double numOfShares = _CurrentValue / _totalShareQuantity;
+                double user_shares = _Amount / numOfShares;
+                double percentvalue = user_shares/(double)(_totalShareQuantity) ;
+                percentvalue *= 100;
+                percentvalue = Math.Floor(percentvalue);
+               // float percentvalue = (_Amount / (_CurrentValue / float(_totalShareQuantity)))/float(_totalShareQuantity) *100;
+                Dictionary<string, object> res = new Dictionary<string, object>() { { "Percentage", percentvalue } };
+                successResponseModel = new SuccessResponse(res, hasData);
+
+            }
+            catch (Exception ex)
+            {
+                //Logger.WriteErrorLog(ex);
+                return new ErrorResponse(ex.Message, HttpStatusCode.BadRequest);
+            }
+
+            return successResponseModel;
+
+        }
         public static object SaveDemoRecord(RequestModel request)
         {
             var test = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(Convert.ToString(request.RequestData));
